801d1afb84f41b6d921be268107b3da023ed1877
marc <marcr.11@t-online.de>
2022-04-06 01:34:13 +0200

    added opt

--- homophonic-encryption/index.py
+++ homophonic-encryption/index.py
@@ -4,46 +4,88 @@ import sys, getopt
 
 
 def main(argv):
-
+    print(argv)
+    opt = False
+    default = True
+    frequencyCharEnglish = []
     print("Input:", argv[0])
 
-    if len(argv) != 1:
+    if len(argv) > 2:
         return
+    if len(argv) == 1:
+        default = True
+    if len(argv) == 2:
+        if argv[1] != ("-e" and "-g") or len(argv) == 1:
+            print("Please chose as an option -g or -e ")
+            return
+        opt = argv[1]
+
     nums = [i for i in range(100)]
 
-    frequencyCharGerman = [
-        6,
-        2,
-        2,
-        5,
-        17,
-        2,
-        3,
-        5,
-        8,
-        1,
-        1,
-        3,
-        2,
-        10,
-        2,
-        1,
-        1,
-        7,
-        7,
-        6,
-        4,
-        1,
-        1,
-        1,
-        1,
-        1,
-    ]
+    if opt == "-g" or default:
+        frequencyCharGerman = [
+            6,
+            2,
+            2,
+            5,
+            17,
+            2,
+            3,
+            5,
+            8,
+            1,
+            1,
+            3,
+            2,
+            10,
+            2,
+            1,
+            1,
+            7,
+            7,
+            6,
+            4,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    if opt == "-e":
+        frequencyCharEnglish = [
+            7,
+            1,
+            2,
+            4,
+            12,
+            1,
+            1,
+            5,
+            7,
+            1,
+            1,
+            4,
+            3,
+            7,
+            8,
+            2,
+            1,
+            6,
+            6,
+            10,
+            3,
+            1,
+            3,
+            1,
+            2,
+            1,
+        ]
 
     assignedNums = []
     y = []
+    frequencyCharChosen = frequencyCharEnglish or frequencyCharGerman
 
-    for j in frequencyCharGerman:
+    for j in frequencyCharChosen:
         while j > 0:
             x = choice(nums)
             nums.remove(x)
@@ -64,7 +106,7 @@ def main(argv):
             charEncrypted += x
         except ValueError:
             print(
-                "You entered something containing numbers... Do not do this. Only enter something of type string."
+                "You entered something containing something that was not char... Do not do this. Only enter something of type string."
             )
             return
 
